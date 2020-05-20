784c25e0254c5918c02e9bf66d166665ca6ec251
Alexandros Theodotou <alex@zrythm.org>
2020-05-20 18:37:55 +0100

    add deploy step

---
+++ scripts/push-package.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# this script pushes the given package to the server
+
+set -e
+
+# 1: package
+# 2: distro
+
+PACKAGE=$1
+DISTRO=$2
+
+SSH_OPTS="ssh -o StrictHostKeyChecking=no"
+SERVER_PACKAGES_DIR="srht@forum.zrythm.org:/home/srht/packages/"
+rsync --rsh="$SSH_OPTS" -rP $PACKAGE srht@forum.zrythm.org:/home/srht/packages/$DISTRO/
