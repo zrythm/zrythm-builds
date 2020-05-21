8fbf771309bc2327e01dd8c0fe3763714900dd55
Alexandros Theodotou <alex@zrythm.org>
2020-05-21 23:51:51 +0100

    various fixes for windows10

--- scripts/make-pkg.sh
+++ scripts/make-pkg.sh
@@ -8,13 +8,16 @@ source zrythm-builds/scripts/common.sh.in
 makefile=zrythm-installer/Makefile
 sed -i -e "s/ZRYTHM_VERSION=.*/ZRYTHM_VERSION=master/" $makefile
 sed -i -e "s/ZRYTHM_PKG_VERSION=.*/ZRYTHM_PKG_VERSION=$zrythm_pkg_ver/" $makefile
-sed -i -e "s,/home/ansible,/home/build,g" $makefile
 sed -i -e "s,ARCH_MXE_ROOT=.*,ARCH_MXE_ROOT=$(pwd)/mxe,g" $makefile
 sed -i -e "s,MXE_ZPLUGINS_CLONE_PATH=.*,MXE_ZPLUGINS_CLONE_PATH=$(pwd)/zplugins,g" $makefile
 sed -i -e "s,MXE_GTK3_CLONE_PATH=.*,MXE_GTK3_CLONE_PATH=$(pwd)/gtk,g" $makefile
 sed -i -e "s,BREEZE_DARK_PATH=.*,BREEZE_DARK_PATH=$(pwd)/breeze-icons/icons-dark,g" $makefile
+sed -i -e "0,90s,/home/ansible,/home/build,g" $makefile
 
 if [ "$distro" = "archlinux" ] || [ "$distro" = "windows10" ]; then
+  pushd zrythm
+  meson subprojects download
+  popd
   pushd zplugins
   meson subprojects download
   popd
