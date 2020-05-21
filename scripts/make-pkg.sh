e7715944e90376b3a9a4034caa3d41c9c7124069
Alexandros Theodotou <alex@zrythm.org>
2020-05-21 08:02:14 +0100

    fix more bugs

--- scripts/make-pkg.sh
+++ scripts/make-pkg.sh
@@ -4,12 +4,17 @@ distro=$1
 
 source zrythm-builds/scripts/common.sh.in
 
+makefile=zrythm-installer/Makefile
+sed -i -e "s/ZRYTHM_VERSION=.*/ZRYTHM_VERSION=master/" $makefile
+sed -i -e "s/ZRYTHM_PKG_VERSION=.*/ZRYTHM_PKG_VERSION=$zrythm_pkg_ver/" $makefile
+sed -i -e "s,/home/ansible,/home/build,g" $makefile
+sed -i -e "s,ARCH_MXE_ROOT=.*,ARCH_MXE_ROOT=/home/build/mxe,g" $makefile
+
 # skip if file already exists and not a tag
 source zrythm-builds/scripts/common.sh.in
 if should_skip_packaging $distro ; then
   exit 0 ;
 fi
 
-zrythm-builds/scripts/prepare.sh
 cd zrythm-installer
 make $distro
--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,8 +3,3 @@
 makefile=zrythm-installer/Makefile
 
 source zrythm-builds/scripts/common.sh.in
-
-sed -i -e "s/ZRYTHM_VERSION=.*/ZRYTHM_VERSION=master/" $makefile
-sed -i -e "s/ZRYTHM_PKG_VERSION=.*/ZRYTHM_PKG_VERSION=$zrythm_pkg_ver/" $makefile
-sed -i -e "s,/home/ansible,/home/build,g" $makefile
-sed -i -e "s,ARCH_MXE_ROOT=.*,ARCH_MXE_ROOT=/home/build/mxe,g" $makefile
