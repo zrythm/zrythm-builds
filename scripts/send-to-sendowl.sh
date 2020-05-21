701792ea081d1863e3d4a5384f77a37cbdf29b55
Alexandros Theodotou <alex@zrythm.org>
2020-05-21 17:47:42 +0100

    start adding sendowl api requests

--- scripts/send-to-sendowl.sh
+++ scripts/send-to-sendowl.sh
@@ -3,4 +3,19 @@
 source ~/.sendowl_credentials
 source zrythm-builds/scripts/common.sh.in
 
-curl -H "Accept: text
+res_file=res.json
+
+get_res () {
+  suffix=$1
+  curl -H "Accept: application/json" "https://$SENDOWL_KEY:$SENDOWL_SECRET@www.sendowl.com/api/v1/$suffix" > res.json
+  sleep 2
+}
+
+bundle_exists () {
+  get_res "packages/search?term=$zrythm_pkg_ver"
+  cat $res_file | jq '.[0]' && return 1 || return 0
+}
+
+# check if a bundle already exists for this version
+bundle_exists=0
+curl -H "Accept: application/json" "https://$SENDOWL_KEY:$SENDOWL_SECRET@www.sendowl.com/api/v1/packages/search?term=0.8.459" > packages.json
